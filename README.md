f256f641c376529565527a0a09e02e25166db7e4
Yusadolat <yusadolat@gmail.com>
2018-09-30 10:09:49 +0100

    Added project Description

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-WhatsApp DicoBot
-=================
+# WhatsApp DicoBot
 
 Powered by Twilio WhatsApp API and Google Dictionary API 🤖
 
+When you search a word, you get the word meaning and it's usage. It's so fast and on point. This is how to beat your grammarian friends who bombard you with jaw threatening words. :laughing:
--- server.js
+++ server.js
@@ -12,51 +12,62 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 app.use(express.static('public'));
 //.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 
 
 app.post('/incoming', (req, res) => {
   const twiml = new MessagingResponse();
   console.log(req.body)
-  if(req.body.Body.toLowerCase().trim()!="hi" && req.body.Body.toLowerCase().trim()!="hello" && req.body.Body.toLowerCase().trim()!="test" && req.body.Body.toLowerCase().trim()!="help"){
-  request('https://googledictionaryapi.eu-gb.mybluemix.net/?define='+req.body.Body, function (error, response, body) {
-    body = JSON.parse(body.trim())
-    console.log('body:', body["word"]);
-    var example;
-    var define;
-    if(_.has(body, 'meaning.adjective') === true){
-      define = body["meaning"]["adjective"][0]["definition"]
-      example = body["meaning"]["adjective"][0]["example"] 
-	  }else if(_.has(body, 'meaning.noun') === true) {
-    
-    define = body["meaning"]["noun"][0]["definition"]
-    example = body["meaning"]["noun"][0]["example"]
+  if (req.body.Body.toLowerCase().trim() != "hi" && req.body.Body.toLowerCase().trim() != "hello" && req.body.Body.toLowerCase().trim() != "test" && req.body.Body.toLowerCase().trim() != "help") {
+    request('https://googledictionaryapi.eu-gb.mybluemix.net/?define=' + req.body.Body, function (error, response, body) {
+      body = JSON.parse(body.trim())
+      console.log('body:', body["word"]);
+      var example;
+      var define;
+      var synonyms;
+      if (_.has(body, 'meaning.adjective') === true) {
+        define = body["meaning"]["adjective"][0]["definition"]
+        example = body["meaning"]["adjective"][0]["example"]
+        synonyms = body["meaning"]["adjective"][0]["synonyms"]
+
+      } else if (_.has(body, 'meaning.noun') === true) {
+
+        define = body["meaning"]["noun"][0]["definition"]
+        example = body["meaning"]["noun"][0]["example"]
+        synonyms = body["meaning"]["noun"][0]["synonyms"]
 
 
-    } else if(_.has(body, 'meaning.verb') === true) {
-    define = body["meaning"]["verb"][0]["definition"]
-    example = body["meaning"]["verb"][0]["example"]
+
+      } else if (_.has(body, 'meaning.verb') === true) {
+        define = body["meaning"]["verb"][0]["definition"]
+        example = body["meaning"]["verb"][0]["example"]
+        synonyms = body["meaning"]["verb"][0]["synonyms"]
 
 
-    }else{
-    console.log("yyy");
-    }
-    
-    // define start here
-//     var example;
-    
-    
-    var msg = twiml.message(`*${body["word"]}*
+      } else {
+        console.log("yyy");
+      }
+
+      // define start here
+      //     var example;
+
+
+      var msg = twiml.message(`*${body["word"]}*
 
 *Definition:* ${define}
-  
- *Example:*  ${example}`);
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
-  });
-  }
-  else{
+
+ *Example:*  ${example}
+
+ *Synonmys:*  ${synonyms.join(', ')}`);
+      res.writeHead(200, {
+        'Content-Type': 'text/xml'
+      });
+      res.end(twiml.toString());
+    });
+  } else {
     var msg = twiml.message(`Hey 👋
 
 I am your Whatsapp Word Assistant (WWA) -a wordtastic bot which helps you with the meaning of any word, right within Whatsapp.
@@ -66,21 +77,22 @@ Try it out - send me a vocabulary you want to know its meaning.
 
 Sponsored by Pronto Afrika (c) 2018.  Developed by Yusadolat
 www.pronto.com.ng Leading Web/App Development Agency.`)
-    res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
+    res.writeHead(200, {
+      'Content-Type': 'text/xml'
+    });
+    res.end(twiml.toString());
   }
-  
+
 });
 
-app.post('/check', function(req, res) {
+app.post('/check', function (req, res) {
   console.log(req.body.Body)
 });
-app.get('*', function(request, response) {
+app.get('*', function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
 
-var listener = app.listen(process.env.PORT  || 3000, function() {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
-
